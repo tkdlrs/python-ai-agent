958b8593ebb48d45d5f887e1d2e0730e48cf0f8c
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-07 07:24:55 -0700

    Continuing on the 'Build an AI Agent in Python' course on Boot.dev. This compeletes 'Chapter 1: LLMs | Lesson 3: Gemini API'.

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import os 
+# 
 from dotenv import load_dotenv
 from google import genai 
 # 
 def main():
-    print("Hello from python-ai-agent!")
-    # 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
-        raise RuntimeError("Unable to load Environment")
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
+    # 
+    user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     # 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.5-flash', 
+        contents=user_prompt
     )
+    # 
+    if response.usage_metadata is None:
+        raise RuntimeError("Response is missing usage_metadata")
+    #
+    print(f"User prompt: {user_prompt}") 
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print("Response:")
     print(response.text)
 
 
