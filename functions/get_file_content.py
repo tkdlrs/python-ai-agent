5dd472e1ab5a76f2022c27866fd47d3eb7642dc1
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-11 18:56:52 -0600

    Reviews the solution files for 'Chapter 2: Function | Lesson 3: Get File Content' in the 'Build an AI Agent in Python' course on Boot.dev.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,27 +1,26 @@
 import os
 # 
+from config import MAX_CHARS
+# 
 def get_file_content(working_directory, file_path):
-    # 
     try:
-        # 
         abs_working_dir = os.path.abspath(working_directory)
-        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        abs_file_path = os.path.normpath(os.path.join(abs_working_dir, file_path))
         # 
-        if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
+        if os.path.commonpath([abs_working_dir, abs_file_path]) != abs_working_dir:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
         # 
-        if not os.path.isfile(target_file):
+        if not os.path.isfile(abs_file_path):
             return f'Error: File not found or is not a regular file: "{file_path}"'
         # 
-        MAX_CHARS = 10000
-        with open(target_file, "r") as f:
-            file_content_string = f.read(MAX_CHARS)
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
             # 
             if f.read(1):
-                file_content_string += f'[...File "{file_path}" truncated as {MAX_CHARS} characters]'
+                content += f'[...File "{file_path}" truncated as {MAX_CHARS} characters]'
             # 
-            return file_content_string
+            return content
         # 
     except Exception as e:
-        return f"Error reading file: {e}"
+        return f'Error reading file "{file_path}": {e}'
 # 
