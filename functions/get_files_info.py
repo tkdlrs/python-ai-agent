2cd841f8cf8d98d09b83b96394a45cf2c8b18813
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-13 16:33:15 -0600

    Completes 'Chapter 3: Function Calling | Lesson 2: Function Declaration' of the 'Build an AI Agent in Python' course on Boot.dev.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,7 @@
 import os
 # 
+from google.genai import types
+# 
 def get_files_info(working_directory, directory="."):
     try:
         abs_working_dir = os.path.abspath(working_directory)
@@ -22,3 +24,18 @@ def get_files_info(working_directory, directory="."):
     except Exception as e:
         return f"Error listing files: {e}"
 # 
+
+# 
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in a specified directory relative to the working directory, providing file size and directory status",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="Directory path to list files from, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
