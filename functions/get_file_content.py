e094064729cfb42dc20b314e6d614febadce325f
tkdlrs <tkd.lsanchez@gmail.com>
2026-03-14 07:56:47 -0600

    Completes 'Chapter 3: Function Calling | Lesson 3: More Declarations' of the 'Build an AI Agent in Python' course on Boot.dev.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,7 @@
 import os
 # 
+from google.genai import types
+# 
 from config import MAX_CHARS
 # 
 def get_file_content(working_directory, file_path):
@@ -23,4 +25,20 @@ def get_file_content(working_directory, file_path):
         # 
     except Exception as e:
         return f'Error reading file "{file_path}": {e}'
+# 
+# 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description=f"Retrieves the content (at most {MAX_CHARS} characters) of a specified file within the working directory",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to read, relative to the working directory",
+            ),
+        },
+        required=["file_path"]
+    ),
+)
 # 
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,5 +1,7 @@
 import os 
 # 
+from google.genai import types
+# 
 def write_file(working_directory, file_path, content):
     try:
         abs_working_dir = os.path.abspath(working_directory)
@@ -21,3 +23,21 @@ def write_file(working_directory, file_path, content):
         return f"Error writing file {abs_file_path}: {e}"
 
 # 
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes text content to a specified file within the working directory (overwriting if the file exists)",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the file to write, relative to the working directory",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Text content to write to the file"
+            )
+        },
+        required=["file_path", "content"] 
+    ),
+)
